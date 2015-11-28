6b3bc65d78dd8839908bd48f920296df4a1aa276
paoesco <pao.esco@outlook.com>
2015-11-28 11:13:18 +0100

    Fix tests

--- src/main/java/com/takeneat/services/Boot.java
+++ src/main/java/com/takeneat/services/Boot.java
@@ -9,9 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Boot {
 
-    private Boot() {
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Boot.class, args);
     }
